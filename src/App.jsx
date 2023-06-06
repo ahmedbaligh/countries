beb9d1883c30157a5c80f25593e777ffc96fcd5e
Hoda <hoda.salim11@gmail.com>
2023-06-06 20:28:58 +0300

    added axios and used it to get the countries API data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-
+import { useState, useEffect } from "react";
 import { Home } from "./pages/Home";
 import { Header } from "./components/Header";
+import axios from "axios";
 
 export function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setCountries(res.data));
+  }, []);
+
   return (
     <>
       <Header />
