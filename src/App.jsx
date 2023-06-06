42cc4d3389946ce8ff26cfbb5905114301b9bb3d
Hoda <hoda.salim11@gmail.com>
2023-06-06 20:48:48 +0300

    Optimized fetching countries API using async await syntax
    Co-authored-by: Ahmed Baligh <ahmedm11@hotmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Home } from "./pages/Home";
-import { Header } from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 import axios from "axios";
+
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 
 export function App() {
   const [countries, setCountries] = useState([]);
+
   useEffect(() => {
-    axios
-      .get("https://restcountries.com/v3.1/all")
-      .then((res) => setCountries(res.data));
+    async function getCountries() {
+      const result = await axios.get("https://restcountries.com/v3.1/all");
+
+      setCountries(result.data);
+    }
+
+    getCountries();
   }, []);
 
   return (
     <>
       <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
