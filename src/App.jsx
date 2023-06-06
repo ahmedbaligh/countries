86f68fbbef2c2a2810828a92756040da9f10b6b4
Hoda <hoda.salim11@gmail.com>
2023-06-06 19:53:03 +0300

    Added Header componenet with working change theme functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import { Home } from './pages/Home';
+import { Home } from "./pages/Home";
+import { Header } from "./components/Header";
 
 export function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,20 +1,22 @@
-import { Flex, Input } from "@chakra-ui/react";
+import { Flex, Input, useColorMode, useColorModeValue } from "@chakra-ui/react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export function InputField({ placeholder, value, onInputChange, ...props }) {
+  const bg = useColorModeValue("gray.100", "blue.700");
+
   return (
     <Flex
       className="search"
       gap={4}
-      bg="white"
       p={3}
       align="center"
       boxShadow="md"
       m={5}
       justify="center"
       {...props}
+      bg={bg}
     >
       <FontAwesomeIcon icon={faMagnifyingGlass} fontSize={20} color="gray" />
 
@@ -23,7 +25,6 @@ export function InputField({ placeholder, value, onInputChange, ...props }) {
         id="search"
         placeholder={placeholder}
         borderRadius={5}
-        variant="unstyled"
         value={value}
         onChange={onInputChange}
       />
