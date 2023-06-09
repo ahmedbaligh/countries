6603846110239250de402ee3f2f66a21fcc37770
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-06-09 10:37:18 +0300

    added the selectMenu for filtering with the styles
    Co-authored-by: Ahmed Baligh <ahmedm11@hotmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
-import { Box } from '@chakra-ui/react';
+import React, { useState } from "react";
+import { Box } from "@chakra-ui/react";
+
+import { SelectMenu } from "../Components/SelectMenu";
+
+const regions = [
+  { value: "africa", label: "Africa" },
+  { value: "americas", label: "Americas" },
+  { value: "asia", label: "Asia" },
+  { value: "europe", label: "Europe" },
+  { value: "oceania", label: "Oceania" },
+];
 
 export function Home() {
-  return <Box>Home</Box>;
+  const [selectedRegion, setSelectedRegion] = useState("");
+
+  const handleDropdownClick = (e) => {
+    setSelectedRegion(e.target.value);
+  };
+
+  return (
+    <Box>
+      <SelectMenu
+        placeholder="Filter By Region"
+        options={regions}
+        value={selectedRegion}
+        onChange={handleDropdownClick}
+      />
+    </Box>
+  );
 }
