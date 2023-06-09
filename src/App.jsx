5d0f01585988e1b0f5bb1ec6b448080008c7b579
Ahmed Baligh <ahmedm11@hotmail.com>
2023-06-09 09:59:22 +0300

    refactor: Move countries fetching logic into Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import axios from 'axios';
 
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
 
 export function App() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    async function getCountries() {
-      const result = await axios.get('https://restcountries.com/v3.1/all');
-
-      setCountries(result.data);
-    }
-
-    getCountries();
-  }, []);
-
   return (
     <>
       <Header />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Grid, Box } from '@chakra-ui/react';
-
-import countries from '../all.json';
+import axios from 'axios';
 
 import CountryCard from '../components/CountryCard';
 import { InputField } from '../components/InputField';
 
 export function Home() {
+  const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    async function getCountries() {
+      const result = await axios.get('https://restcountries.com/v3.1/all');
+
+      setCountries(result.data);
+    }
+
+    getCountries();
+  }, []);
 
   return (
     <Box>
