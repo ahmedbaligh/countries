086de76e7321fb4b2328751b59972d0507dc5b03
Ahmed Baligh <ahmedm11@hotmail.com>
2023-06-13 19:45:50 +0300

    feat: Handle different states variations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Grid, Box } from "@chakra-ui/react";
-import axios from "axios";
 
 import CountryCard from "../components/CountryCard";
 import { InputField } from "../components/InputField";
 import { SelectMenu } from "../components/SelectMenu";
+
+import { useFetch } from "../hooks/useFetch";
 
 const regions = [
   { value: "africa", label: "Africa" },
   { value: "americas", label: "Americas" },
   { value: "asia", label: "Asia" },
   { value: "europe", label: "Europe" },
-  { value: "oceania", label: "Oceania" },
+  { value: "oceania", label: "Oceania" }
 ];
 
 export function Home() {
-  const [countries, setCountries] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedRegion, setSelectedRegion] = useState("");
 
-  const handleDropdownClick = (e) => {
+  const { data: countries, isLoading, error } = useFetch("https://restcountries.com/v3.1/all");
+
+  const handleDropdownClick = e => {
     setSelectedRegion(e.target.value);
   };
 
-  useEffect(() => {
-    async function getCountries() {
-      const result = await axios.get("https://restcountries.com/v3.1/all");
+  const filteredCountries = countries
+    ? countries.filter(country => {
+        const region = regions.find(region => region.value === selectedRegion);
 
-      setCountries(result.data);
-    }
+        const includesName = country.name.common.toLowerCase().includes(searchQuery.toLowerCase().trim());
+        const inRegion = region ? region.label === country.region : true;
 
-    getCountries();
-  }, []);
-
-  const filteredCountries = countries.filter((country) => {
-    const region = regions.find((region) => region.value === selectedRegion);
-
-    const includesName = country.name.common
-      .toLowerCase()
-      .includes(searchQuery.toLowerCase().trim());
-    const inRegion = region ? region.label === country.region : true;
-
-    return includesName && inRegion;
-  });
+        return includesName && inRegion;
+      })
+    : null;
 
   return (
     <Box>
       <InputField
         placeholder="Type a keyword to search with"
         value={searchQuery}
-        onInputChange={(e) => setSearchQuery(e.target.value)}
+        onInputChange={e => setSearchQuery(e.target.value)}
       />
 
       <SelectMenu
@@ -60,9 +52,15 @@ export function Home() {
       />
 
       <Grid templateColumns="repeat(4, 1fr)" gap="20" bgColor="gray.800">
-        {filteredCountries.map((country) => (
-          <CountryCard key={country.cca2} country={country} />
-        ))}
+        {error ? (
+          <p>{error.message}</p>
+        ) : isLoading ? (
+          <p>Loading...</p>
+        ) : countries.length === 0 ? (
+          <p>No countries found</p>
+        ) : (
+          filteredCountries.map(country => <CountryCard key={country.cca2} country={country} />)
+        )}
       </Grid>
     </Box>
   );
