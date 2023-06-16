fd9ab47285683e6708c33bbdfd9319c12b427c5f
Hoda <hoda.salim11@gmail.com>
2023-06-16 15:57:13 +0300

    feat: Added dynamic country details from the API

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -3,14 +3,28 @@ import { useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
+import { useFetch } from '../hooks/useFetch';
+
 export function CountryDetails() {
   const { countryID } = useParams();
+
+  const { data, isLoading, error } = useFetch(`https://restcountries.com/v3.1/alpha/${countryID}`);
+  const buttonBg = useColorModeValue('white', 'blue.700');
+
+  if (isLoading) return <p>Loading...</p>;
+
+  const country = data[0];
+  const languages = Object.keys(country.languages);
+  const nativeName = country.name.nativeName[languages[0]].common;
+  const currency = Object.keys(country.currencies)[0];
+  const currencyName = country.currencies[currency].name;
+  const formmatedLanguages = Object.values(country.languages).join(', ');
 
   return (
     <Container flex={1} py="14" gap={{ base: 20, lg: 16 }}>
       <Button
         leftIcon={<FontAwesomeIcon icon={faArrowLeft} />}
-        bg={useColorModeValue('white', 'blue.700')}
+        bg={buttonBg}
         shadow="sm"
         w="32"
         borderRadius="sm"
@@ -20,26 +34,26 @@ export function CountryDetails() {
       </Button>
 
       <Flex gap={[12, null, null, '24']} align={{ lg: 'center' }} flexDir={['column', null, 'row']}>
-        <Image src="https://flagcdn.com/jo.svg" alt="Belgium" minW="0" flex={1} />
+        <Image src={country.flags.png} alt="Belgium" minW="0" flex={1} />
 
         <Flex minW="0" flex={1} flexDir="column" gap="8">
           <Heading as="h2" size="lg">
-            Belgium
+            {country.name.common}
           </Heading>
 
           <Flex justify="space-between" gap={{ base: 8, lg: 14 }} flexDir={['column', null, null, 'row']}>
             <Flex flexDir="column" gap="1.5">
-              <CountryDetail label="Native Name" value="Belgiē" />
-              <CountryDetail label="Population" value="11,319,51" />
-              <CountryDetail label="Region" value="Europe" />
-              <CountryDetail label="Sub Region" value="Western Europe" />
-              <CountryDetail label="Capital" value="Brussels" />
+              <CountryDetail label="Native Name" value={nativeName} />
+              <CountryDetail label="Population" value={country.population.toLocaleString()} />
+              <CountryDetail label="Region" value={country.region} />
+              <CountryDetail label="Sub Region" value={country.subregion} />
+              <CountryDetail label="Capital" value={country.capital[0]} />
             </Flex>
 
             <Flex flexDir="column" gap="1.5">
-              <CountryDetail label="Top Level Doamn" value=".be" />
-              <CountryDetail label="Currencies" value="Euro" />
-              <CountryDetail label="Languages" value="Dutch, French, German" />
+              <CountryDetail label="Top Level Doamn" value={country.tld[0]} />
+              <CountryDetail label="Currencies" value={currencyName} />
+              <CountryDetail label="Languages" value={formmatedLanguages} />
             </Flex>
           </Flex>
 
@@ -54,8 +68,19 @@ export function CountryDetails() {
             </Heading>
 
             <Flex gap="2" flexWrap="wrap">
-              {[1, 2, 3].map((_, i) => (
-                <BorderCountryButton key={i}>France</BorderCountryButton>
+              {country.borders.map(countryCode => (
+                <Button
+                  key={countryCode}
+                  bg={buttonBg}
+                  shadow="sm"
+                  borderRadius="sm"
+                  fontWeight="300"
+                  minWidth={{ base: 24, lg: '28' }}
+                  h="8"
+                  fontSize={{ base: 'sm', lg: 'md' }}
+                >
+                  {countryCode}
+                </Button>
               ))}
             </Flex>
           </Flex>
@@ -72,20 +97,6 @@ const CountryDetail = ({ label, value }) => (
       {value}
     </Text>
   </Text>
-);
-
-const BorderCountryButton = ({ children }) => (
-  <Button
-    bg={useColorModeValue('white', 'blue.700')}
-    shadow="sm"
-    borderRadius="sm"
-    fontWeight="300"
-    minWidth={{ base: 24, lg: '28' }}
-    h="8"
-    fontSize={{ base: 'sm', lg: 'md' }}
-  >
-    {children}
-  </Button>
 );
 
 /* 
